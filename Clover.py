1e21f7d22056fe94ff7f521f24b54a9604deb3f9
ksayles <ksayles@ucsc.edu>
2018-01-13 16:19:03 -0800

    Fixed list.
    List was not copying each dict entry correctly, found I needed to use
    dict.copy because list was only referenced.

--- Clover.py
+++ Clover.py
@@ -1,6 +1,7 @@
 def parse_flatfile(datafilename, formatfilename):
     output = {} #Dictionary output
-    data = []
+    outputList = []
+
     with open(datafilename) as dataFile, open(formatfilename) as formatFile:
         for dataLine in dataFile:
             dataLine = dataLine.split() #To seperate values I need to work with
@@ -8,13 +9,11 @@ def parse_flatfile(datafilename, formatfilename):
             if len(dataLine) < 3: #Some lines the valid value and count value are not seperated by space bar so this fixes that
                 dataLine.append(dataLine[1][1:])
                 dataLine[1] = dataLine[1][:1]
-            print(dataLine)
 
             output['name'] = dataLine[0]
-            output['valid'] = dataLine[1]
-            output['count'] = dataLine[2]
-
-            data.append(output) #Create list of the key-value pairs.
+            output['valid'] = bool(dataLine[1])
+            output['count'] = int(dataLine[2])
+            outputList.append(output.copy()) #Create list of the key-value pairs.
 
 
 
@@ -23,7 +22,7 @@ def parse_flatfile(datafilename, formatfilename):
                 #formatLine = formatLine.rstrip()  # get rid of '\n' showing up in list
                 #formatLine = formatLine.split(',')
                 #print(formatLine)
-    print(data)
+    print(outputList)
 
 parse_flatfile('datafile.txt','formatfile.csv')
 
