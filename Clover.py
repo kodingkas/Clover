69e8191935847e22861af642ff0e8c1233b4f409
ksayles <ksayles@ucsc.edu>
2018-01-18 09:00:08 -0800

    Quick Changes
    Variable name changes, comments added, and unnecessary code removed.

--- Clover.py
+++ Clover.py
@@ -2,19 +2,19 @@ import csv
 
 def parse_flatfile(datafilename, formatfilename):
     output = {} #Dictionary output
-    outputList = []
-    colNames = []
+    outputList = [] #Want a list of dictionaries
+    colNames = [] #Will be the key names in output dictionary
 
     with open(datafilename) as dataFile, open(formatfilename) as formatFile:
-        fileformat = csv.reader(formatFile)
+        formatCSV = csv.reader(formatFile)
 
-        for key in fileformat: #getting the column names in list to work with later.
-            if key[0] == 'column name':
+        for key in formatCSV: #getting the column names in list to work with later.
+            if key[0] == 'column name': #skipping first row
                 continue
             colNames.append(key[0])
 
         for dataLine in dataFile:
-            dataLine = dataLine.split() #To seperate values I need to work with
+            dataLine = dataLine.split() #To separate values I need to work with
 
             if len(dataLine) < 3: #Some lines the valid value and count value are not seperated by space bar so this fixes that
                 dataLine.append(dataLine[1][1:])
@@ -25,13 +25,6 @@ def parse_flatfile(datafilename, formatfilename):
             output[colNames[2]] = int(dataLine[2])
             outputList.append(output.copy()) #Create list of the key-value pairs.
 
-
-
-        #for data in file_data:
-            #for formatLine in formatFile:
-                #formatLine = formatLine.rstrip()  # get rid of '\n' showing up in list
-                #formatLine = formatLine.split(',')
-                #print(formatLine)
     print(outputList)
 
 parse_flatfile('datafile.txt','formatfile.csv')
